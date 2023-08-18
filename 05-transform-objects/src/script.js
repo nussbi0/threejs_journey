4821322da96f40bd2ce9cc479841c18c3fdc7559
David Nussbaumer <dave.nussbaumer@gmail.com>
2023-08-18 14:29:06 +0200

    up to 07

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,14 +9,41 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
 
-mesh.position.set(0.7, -0.6, 1)
-mesh.scale.set(2, 0.5, 0.5)
-mesh.rotation.y = Math.PI / 4
+// mesh.position.set(0.7, -0.6, 1)
+// mesh.scale.set(2, 0.5, 0.5)
+// mesh.rotation.y = Math.PI / 4
+
+const group = new THREE.Group()
+group.position.set(0, 1, 0)
+group.scale.set(1, 2, 1)
+group.rotation.set(0, 1, 0)
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.set(-2, 0, 0)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.set(2, 0, 0)
+group.add(cube3)
+
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2)
@@ -34,9 +61,9 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(0, 0, 3)
+camera.position.set(0, 0, 5)
 scene.add(camera)
-
+camera.lookAt(group.position)
 
 /**
  * Renderer
